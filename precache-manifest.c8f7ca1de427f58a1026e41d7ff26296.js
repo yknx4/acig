97692be6502c4293b2e80c6362df87514aced109
Ale Ornelas <yknx.4.b@gmail.com>
2020-09-03 20:53:32 -0500

    Updates

---
+++ precache-manifest.c8f7ca1de427f58a1026e41d7ff26296.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efd991b71a2185f7966d83fbfe6773e",
+    "url": "/acig/index.html"
+  },
+  {
+    "revision": "8b7129f1fb7ec2b4451c",
+    "url": "/acig/static/css/2.c37a399a.chunk.css"
+  },
+  {
+    "revision": "3c6bbd9c108d9b1b87ec",
+    "url": "/acig/static/css/main.22a32d76.chunk.css"
+  },
+  {
+    "revision": "8b7129f1fb7ec2b4451c",
+    "url": "/acig/static/js/2.eabe3481.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/acig/static/js/2.eabe3481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6bbd9c108d9b1b87ec",
+    "url": "/acig/static/js/main.7bcaadef.chunk.js"
+  },
+  {
+    "revision": "9296fdf744dca307349a",
+    "url": "/acig/static/js/runtime-main.b35bf697.js"
+  },
+  {
+    "revision": "64bdcedb591dec0cda41a29c9d83b691",
+    "url": "/acig/static/media/leaf-logo.64bdcedb.png"
+  }
+]);
