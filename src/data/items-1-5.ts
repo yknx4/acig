2fda936dd19c48e6d422f42349cb19b274e323a9
Jade Ornelas Figueroa <afigueroa@goamv.com>
2020-09-30 10:42:29 -0500

    chore(items): start adding new 1.5 items

---
+++ src/data/items-1-5.ts
@@ -0,0 +1,41 @@
+/**
+ * TODO: Eventually replace with real fillings, this is meanwhile to start feeding the update 1.5.0 items
+ */
+
+import { Insertable, Recipe } from "../definitions/acnh";
+
+ export const temp1_5Items: Array<Partial<Insertable>> = [
+  {"Name": "small pumpkin sprout", "Internal ID": "0BEC"},
+  {"Name": "medium pumpkit sprout", "Internal ID": "0BED"},
+  {"Name": "large pumpkin sprout", "Internal ID": "110A"},
+  {"Name": "orange pumpkin", "Internal ID": "11AD"},
+  {"Name": "yellow pumpkin", "Internal ID": "11AE"},
+  {"Name": "GREEN Pumpkin", "Internal ID": "11AF"},
+  {"Name": "white pumpkin", "Internal ID": "11B0"},
+  {"Name": "Jack's robe", "Internal ID": "32BB"},
+  {"Name": "raggedy outfit", "Internal ID": "32BC"},
+  {"Name": "mage' dress", "Internal ID": "32BD"},
+  {"Name": "flashy animal costume", "Internal ID": "32BE"},
+  {"Name": "lollipop", "Internal ID": "338C"},
+  {"Name": "candy", "Internal ID": "338D"},
+  {"Name": "Body-Paint Costume Tips (ATM feature activator)", "Internal ID": "33C8"},
+  {"Name": "Exploring New Eye colors (ATM feature activator)", "Internal ID": "33C9"},
+  {"Name": "Ring-Con", "Internal ID": "33ED"},
+  {"Name": "Jack's portrait", "Internal ID": "3429"}
+ ]
+ export const temp1_5IRecipes: Array<Partial<Recipe>> = [
+  {"Name": "spooky table", "Internal ID": "00CA"},
+  {"Name": "spooky arch", "Internal ID": "00CD"},
+  {"Name": "spooky scarecrow", "Internal ID": "00CE"},
+  {"Name": "spooky tower", "Internal ID": "00CF"},
+  {"Name": "spooky carriage", "Internal ID": "00D0"},
+  {"Name": "spooky lantern", "Internal ID": "00D1"},
+  {"Name": "spooky chair", "Internal ID": "00D2"},
+  {"Name": "spooky lantern set", "Internal ID": "00D4"},
+  {"Name": "spooky candy set", "Internal ID": "02DC"},
+  {"Name": "spooky standing lamp", "Internal ID": "02DD"},
+  {"Name": "spooky garland", "Internal ID": "02DE"},
+  {"Name": "spooky table setting", "Internal ID": "02DF"},
+  {"Name": "spooky wand", "Internal ID": "02E0"},
+  {"Name": "spooky fence", "Internal ID": "02E1"}
+ ]
