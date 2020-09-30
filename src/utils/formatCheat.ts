e2a1d8fa9c17098f40203e266899c3a014e07d2b
Jade Ornelas Figueroa <afigueroa@goamv.com>
2020-09-30 10:42:06 -0500

    build(nodejs): upgrade to 14.13

--- src/utils/formatCheat.ts
+++ src/utils/formatCheat.ts
@@ -4,7 +4,8 @@ import { Insertable, Recipe, VariableInsertable } from '../definitions/acnh';
 import ot from '../data/output_template.json'
 import { isRecipe, isVariableInsertable } from './items';
 
-const outputTemplate = ot as string[][]
+const outputTemplate = ot
+type Player = keyof typeof ot
 
 function recipeCheat(item: Recipe): string[] {
   return [decimalToHex(realId(item)), '000016A2']
@@ -14,9 +15,9 @@ function itemCheat(item: Insertable | VariableInsertable): string[] {
   return [isVariableInsertable(item) ? parseVariation(item["Variant ID"]) : '00000000', decimalToHex(realId(item))]
 }
 
-export function formatCheat(item: Insertable | VariableInsertable, indexAsString: string) {
+export function formatCheat(item: Insertable | VariableInsertable, indexAsString: string, player: Player = "Player 1") {
   const index = parseInt(indexAsString, 10);
-  const template = outputTemplate[index];
+  const template = outputTemplate[player][index];
   const [thirdField, fourthField] =  isRecipe(item) ? recipeCheat(item) : itemCheat(item)
   return `${template[0]} ${template[1]} ${thirdField} ${fourthField}\n`;
 }
